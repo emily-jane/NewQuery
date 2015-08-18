217130e1c441ebc5277b6601cb19266117b1d2c0
lewis <ljones140@gmail.com>
2015-08-18 17:31:27 +0100

    refactoring Jquery

--- src/appJquery.js
+++ src/appJquery.js
@@ -4,32 +4,27 @@ $( document ).ready(function() {
   var setTempColour = function(){
     $("#temperature").html(thermostat.temperature);
     $("#temperature").css("color", thermostat.colour);
-
-  }):
+  };
 
   setTempColour();
 
   $("#increase").click(function(){
     thermostat.increaseTemperature();
     setTempColour();
-
   });
 
   $("#decrease").click(function(){
       thermostat.decreaseTemperature();
       setTempColour();
-
   });
 
   $("#reset").click(function(){
       thermostat.resetTemperature();
       setTempColour();
-
   });
 
   $("#power_save").click(function(){
     ($("#power_save").is(":checked")) ? thermostat.powerSavemodeOn() : thermostat.powerSavemodeOff();
-
   });
 
 });

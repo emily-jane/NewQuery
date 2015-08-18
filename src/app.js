906e34b39fae76b9e750195fdc6bbc93c9216375
lewis <ljones140@gmail.com>
2015-08-18 15:28:08 +0100

    stage 3 complete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 thermostat = new Thermostat();
 
+document.getElementById('temperature').style.color = thermostat.colour;
+
 var temperature = document.getElementById('temperature');
   temperature.innerHTML = thermostat.temperature;
 
@@ -7,21 +9,25 @@ var upTemperature = document.getElementById('increase');
   upTemperature.onclick = function() {
     thermostat.increaseTemperature();
     temperature.innerHTML = thermostat.temperature;
+    document.getElementById('temperature').style.color = thermostat.colour;
   };
 
 var downTemperature = document.getElementById('decrease');
   downTemperature.onclick = function() {
     thermostat.decreaseTemperature();
     temperature.innerHTML = thermostat.temperature;
+    document.getElementById('temperature').style.color = thermostat.colour;
   };
 
 var resetTemperature = document.getElementById('reset');
   resetTemperature.onclick = function() {
     thermostat.resetTemperature();
     temperature.innerHTML = thermostat.temperature;
+    document.getElementById('temperature').style.color = thermostat.colour;
   };
 
 var powerSaveStatus = document.getElementById('power_save');
   powerSaveStatus.onclick = function() {
     (powerSaveStatus.checked) ? thermostat.powerSavemodeOn() : thermostat.powerSavemodeOff();
 };
+

df736d90a55191b743eba8db2c22bbe5604f1415
lewis <ljones140@gmail.com>
2015-08-19 17:33:35 +0100

    refactored buttons

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -11,73 +11,67 @@ describe('Feature tests',function(){
   });
 
   it('increases temperature with up button', function() {
-    $("button:eq(0)").click();
+    $('button[data-temp-control="up"]').click();
     expect('#temperature').toContainText('21');
   });
 
   it('decreases temperature with down button', function() {
-    $("button:eq(1)").click();
+    $('button[data-temp-control="down"]').click();
     expect('#temperature').toContainText('19');
   });
 
   it('temperature can go up and down consistently', function() {
-    $("button:eq(1)").click();
+    $('button[data-temp-control="down"]').click();
     expect('#temperature').toContainText('19');
-    $("button:eq(0)").click();
+    $('button[data-temp-control="up"]').click();
     expect('#temperature').toContainText('20');
   });
 
   it('temperature can be reset from higher', function() {
-    expect('#temperature').toContainText('20');
-    $("button:eq(0)").click();
+    $('button[data-temp-control="up"]').click();
     expect('#temperature').toContainText('21');
-    $("button:eq(2)").click();
+    $('button[data-temp-control="reset"]').click();
     expect('#temperature').toContainText('20');
   });
 
   it('temperature can be reset from lower', function() {
-    expect('#temperature').toContainText('20');
-    $("button:eq(1)").click();
+    $('button[data-temp-control="down"]').click();
     expect('#temperature').toContainText('19');
-    $("button:eq(2)").click();
+    $('button[data-temp-control="reset"]').click();
     expect('#temperature').toContainText('20');
   });
 
   it('max 25 with powersave', function() {
-    expect('#temperature').toContainText('20');
     $('input[type="checkbox"]').prop('checked', true);
     for (i = 0; i < 6; i++) {
-      $("button:eq(0)").click();
+      $('button[data-temp-control="up"]').click();
     }
     expect('#temperature').toContainText('25');
   });
 
   it('min temp is 10', function() {
-    expect('#temperature').toContainText('20');
     $('input[type="checkbox"]').click();
     for (i = 0; i < 12; i++) {
-      $("button:eq(1)").click();
+      $('button[data-temp-control="down"]').click();
     }
     expect('#temperature').toContainText('10');
   });
 
   it('max 32 with powersave off', function() {
-    expect('#temperature').toContainText('20');
-    $('input[type="checkbox"]').click();
+    $('input[data-temp-control="powersave"]').click();
     for (i = 0; i < 15; i++) {
-      $("button:eq(0)").click();
+      $('button[data-temp-control="up"]').click();
     }
     expect('#temperature').toContainText('32');
   });
 
   it('powersave set resets to default max', function() {
-    expect('#temperature').toContainText('20');
     $('input[type="checkbox"]').click();
     for (i = 0; i < 15; i++) {
-      $("button:eq(0)").click();
+      $('button[data-temp-control="up"]').click();
     }
     expect('#temperature').toContainText('32');
-    $('input[type="checkbox"]').click();
+    $('input[data-temp-control="powersave"]').click();
     expect('#temperature').toContainText('25');
   });
 
@@ -87,26 +81,40 @@ describe('Feature tests',function(){
   });
 
   it('is red at >= 25', function() {
-    expect($('a').css("color")).toBe('rgb(0, 0, 238)')
-    expect('#temperature').toContainText('20');
-    $('input[type="checkbox"]').click();
+    $('input[data-temp-control="powersave"]').click();
     for (i = 0; i < 5; i++) {
-      $("button:eq(0)").click();
+      $('button[data-temp-control="up"]').click();
     }
     expect('#temperature').toContainText('25');
     expect($('#temperature').css("color")).toBe('rgb(255, 0, 0)')
   });
 
   it('is green at < 18', function() {
-    expect($('a').css("color")).toBe('rgb(0, 0, 238)')
-    expect('#temperature').toContainText('20');
     $('input[type="checkbox"]').click();
     for (i = 0; i < 2; i++) {
-      $("button:eq(1)").click();
+      $('button[data-temp-control="down"]').click();
     }
     expect('#temperature').toContainText('18');
     expect($('#temperature').css("color")).toBe('rgb(255, 255, 0)')
   });
 
+  xit('has a weather element', function() {
+    console.log("#weather")
+    expect('#weather').toContainText('City: London Weather: 20.12scattered clouds');
+  });
 });
+
+// describe('Feature tests',function(){
+//   it('calls weather API', funtion() {
+//   spyOn($, "getJSON");
+//   weatherCall();
+
+
+
+//   });
+
+// }):
+
+
+
 
--- src/appJquery.js
+++ src/appJquery.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function(){
   thermostat = new Thermostat();
 
   var setTempColour = function(){
@@ -6,25 +6,39 @@ $( document ).ready(function() {
     $("#temperature").css("color", thermostat.colour);
   };
 
-  setTempColour();
+  var showWeather = function(result){
+   $("#weather").html(("City: "+result.name + " Weather: "+result.main.temp
+      +result.weather[0].description ));
+   var weatherImage = (result.weather[0].icon)
+   $("#weatherImage").html("<img src='http://openweathermap.org/img/w/"+weatherImage+".png' />")
+  };
 
-  $("button:eq(0)").click(function() {
+  var weatherCall = function(){
+     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=b5d17c3afa857ebbab97b63ae2330336",function(result){
+    showWeather(result);
+    });
+  };
+
+  setTempColour();
+  weatherCall();
+
+  $('button[data-temp-control="up"]').click(function(){
     thermostat.increaseTemperature();
     setTempColour();
   });
 
-  $("button:eq(1)").click(function() {
+  $('button[data-temp-control="down"]').click(function(){
     thermostat.decreaseTemperature();
     setTempColour();
   });
 
-  $("button:eq(2)").click(function() {
+  $('button[data-temp-control="reset"]').click(function(){
     thermostat.resetTemperature();
     setTempColour();
   });
 
-  $('input[type="checkbox"]').eq(0).click(function() {
-    ($('input[type="checkbox"]').eq(0).is(":checked")) ? thermostat.powerSavemodeOn() : thermostat.powerSavemodeOff();
+  $('input[data-temp-control="powersave"]').click(function(){
+    ($('input[data-temp-control="powersave"]').is(":checked")) ? thermostat.powerSavemodeOn() : thermostat.powerSavemodeOff();
     setTempColour();
   });
 });
